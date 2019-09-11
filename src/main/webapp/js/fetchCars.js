6eb622cc735dac54c59d73c1439091ac55d68faa
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-09-11 16:45:58 +0200

    fetchCars.js - sorting - Martin

--- src/main/webapp/js/fetchCars.js
+++ src/main/webapp/js/fetchCars.js
@@ -4,47 +4,45 @@
 
 //constants
 const URL = "/CAone/api/cars/all";
-
 /**
  * Making table of all cars and utils
  */
 function getAllCars(e) {
     e.preventDefault();
-//    console.log(URL);
+    console.log(e.target.innerText);
     fetch(URL)
             .then(res => res.json())
             .then(data => {
-//                console.log("data: " + data);
-
+                var headerCellText = e.target.innerText;
+                if (headerCellText === "price") {
+                    data.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+                } else if (headerCellText === "make") {
+                    data.sort((a, b) => a.make.localeCompare(b.make));
+                } else if (headerCellText === "carID" ||
+                       headerCellText === "Cars" ) {
+                    data.sort((a, b) => parseInt(a.carID) - parseInt(b.carID));
+                } else {
+                    data.sort((a, b) => parseInt(a.year) - parseInt(b.year));
+                }
                 /**
                  * jsonList2Table() is in tables.js
                  * It converts a json-list into a html table
                  * arguments: a jsonlist and the tag identifier
                  */
                 jsonList2Table(data, "#content");
+                addEvents();
             });
 }
 
-function sort(e){
-    e.preventDefault();
-        fetch(URL)
-            .then(res => res.json())
-            .then(data => {
-//                data.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
-                data.sort((a, b) => a.make.localeCompare(b.make));
-                /**
-                 * jsonList2Table() is in tables.js
-                 * It converts a json-list into a html table
-                 * arguments: a jsonlist and the tag identifier
-                 */
-                jsonList2Table(data, "#content");
-            });
+function addEvents() {
+    document.querySelector("#make").addEventListener("click", getAllCars);
+    document.querySelector("#year").addEventListener("click", getAllCars);
+    document.querySelector("#carID").addEventListener("click", getAllCars);
+    document.querySelector("#price").addEventListener("click", getAllCars);
 }
 
-
-// Cars button eventlistener
+//eventlistener
 document.querySelector("#carPage").addEventListener("click", getAllCars);
-document.querySelector("#make").addEventListener("click", sort);
 console.log("check");
 
 
