435e90f6b9eb6f0fce21572198f121fc7f8ef3ee
Simon Kristopher Kruse Bentzen <Simonkruse2@gmail.com>
2019-09-11 10:20:56 +0200

    Added JokeFacade and JokeResource with endpoints
    Still no tests, those are coming later today.

---
+++ src/main/java/facades/JokeFacade.java
@@ -0,0 +1,77 @@
+package facades;
+
+import entities.Joke;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class JokeFacade {
+
+    private static JokeFacade instance;
+    private static EntityManagerFactory emf;
+
+    //Private Constructor to ensure Singleton
+    private JokeFacade() {
+    }
+
+    /**
+     *
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static JokeFacade getJokesFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new JokeFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public Joke addJoke(String joke, String jokeType, String reference) {
+        Joke j = new Joke(jokeType, jokeType, reference);
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(j);
+            em.getTransaction().commit();
+            return j;
+        } finally {
+            em.close();
+        }
+    }
+
+    public Joke findByID(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Joke joke = em.find(Joke.class, id);
+            return joke;
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Joke> allJokes() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery<Joke> query
+                    = em.createQuery("Select j from Joke j", Joke.class);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -14,15 +14,15 @@ public class ApplicationConfig extends Application {
     }
 
     /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
+     * Do not modify addRestResourceClasses() method. It is automatically
+     * populated with all resources defined in the project. If required, comment
+     * out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.CarResource.class);
         resources.add(rest.GroupMemberResource.class);
- }
-    
+        resources.add(rest.JokeResource.class);
+
+    }
 }
