d6d8ed466ba3bff3b899baa105862101eb5c882d
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-09-11 10:33:09 +0200

    tables.js - test af functioner til at danne html-tabeller

--- src/main/webapp/js/tables.js
+++ src/main/webapp/js/tables.js
@@ -1,10 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * 
  */
 
-
-function test(){
+function test() {
     console.log("Jeg blev stadig aktiveret");
+}
+
+
+//table.push("<thead><tr><th>Title:</th><th>Year:</th></th></tr></thead>");
+
+function list2Table(list){
+    
+    makeHeader();
+    makeBody();
+    
 }
