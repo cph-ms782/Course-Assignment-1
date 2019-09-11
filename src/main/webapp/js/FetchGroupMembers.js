ae74bdd1c24794faefb32f096f32e829c1428ce3
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2019-09-11 12:33:20 +0200

    FetchGroupMembers.js - Changed to work with regular index page, deleted indexTEST - Frederik

--- src/main/webapp/js/FetchGroupMembers.js
+++ src/main/webapp/js/FetchGroupMembers.js
@@ -1,5 +1,5 @@
 //Constants for all DOM elements im manipulating
-const RELOAD = document.querySelector("#reload");
+const GROUPPAGE = document.querySelector("#groupPage");
 const CONTENT = document.querySelector("#content");
 
 /**
@@ -11,16 +11,22 @@ const CONTENT = document.querySelector("#content");
  */
 function groupMembersToTable(groupMembers) {
     var tableinfo = groupMembers.map(x => "<tr><td>" + x.name + "</td><td>" + x.studentID + "</td><td>" + x.color + "</td></tr>");
+    
     tableinfo.unshift("<table id=\"indextable\" class=\"table\"><tr><th onclick=\"sortByLetters(0)\">Name</th>\n\
     <th onclick=\"sortByNumbers(1)\">Student ID</th>\n\
     <th onclick=\"sortByLetters(2)\">Color</th></tr>");
+    
     tableinfo.push("</table>");
     return tableinfo.join('');
 }
 
-RELOAD.onclick = function (e) {
+GROUPPAGE.onclick = function (e) {
     e.preventDefault();
-    let url = "https://www.sandersolutions.dk/CAone/api/groupmembers/all";
+    //deployed url
+    //let url = "https://www.sandersolutions.dk/CAone/api/groupmembers/all";
+    
+    //local url
+    let url = "http://localhost:8080/caone/api/groupmembers/all";
     fetch(url)
             .then(res => res.json()) //in flow1, just do it
             .then(data => {
