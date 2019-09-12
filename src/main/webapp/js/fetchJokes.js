575aa91d59991a38928e0a198fc6ff6f5eaa1fcc
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2019-09-12 09:53:17 +0200

    fetchJokes.js - working on jokeByID - Frederik

--- src/main/webapp/js/fetchJokes.js
+++ src/main/webapp/js/fetchJokes.js
@@ -1,6 +1,8 @@
 //Constants for all DOM elements im manipulating
 const JOKESPAGE = document.querySelector("#jokePage");
 const JOKECONTENT = document.querySelector("#content");
+const JOKEBYID = document.querySelector("#jokeByIdButton");
+const JOKEID = document.querySelector("#jokeId");
 
 /**
  * 
@@ -16,8 +18,9 @@ function jokesToTable(jokes) {
 };
 
 function jokeByIdButton(){
-    return "<form> Joke ID: <input type=\"text\" id=\"pricetext\"> <input type=\"submit\" id=\"jokeByIdButton\" value=\"Get a joke\"></form>"
+    return "<form> Joke ID: <input type=\"text\" id=\"jokeId\"> <input type=\"submit\" id=\"jokeByIdButton\" value=\"Get a joke\"></form>"
 }
+
 
 JOKESPAGE.onclick = function (e) {
     e.preventDefault();
@@ -34,3 +37,20 @@ JOKESPAGE.onclick = function (e) {
                  Now, just build your DOM changes using the data*/
             });
 };
+
+JOKEBYID.onclick = function(e) {
+    e.preventDefault();
+    let jokeId = JOKEID.value;
+    let url = "/CAone/api/joke/" + jokeId;
+    fetch(url)
+            .then(res => res.json()) //in flow1, just do it
+            .then(data => {
+                // Inside this callback, and only here, the response data is available
+                console.log("data", data);
+                JOKECONTENT.innerHTML = "<p>" + data.joke + "</p>";
+                JOKECONTENT.innerHTML += jokeByIdButton();
+                /* data now contains the response, converted to JavaScript
+                 Observe the output from the log-output above
+                 Now, just build your DOM changes using the data*/
+            });
+}
