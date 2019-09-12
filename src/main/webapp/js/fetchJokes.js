36339de596200902fd6ea5b6aedf7b976c85d543
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2019-09-12 09:29:16 +0200

    fetchJokes.js - Added jokeByIDButton - Frederik

--- src/main/webapp/js/fetchJokes.js
+++ src/main/webapp/js/fetchJokes.js
@@ -13,18 +13,22 @@ function jokesToTable(jokes) {
     tableContent.unshift("<table class=\"table\" border=\"1\"><tr><th>Joke</th><th>JokeID</th><th>Type</th><th>Reference</th></tr>");
     tableContent.push("</table>");
     return tableContent.join('');
+};
+
+function jokeByIdButton(){
+    return "<form> Joke ID: <input type=\"text\" id=\"pricetext\"> <input type=\"submit\" id=\"jokeByIdButton\" value=\"Get a joke\"></form>"
 }
-;
 
 JOKESPAGE.onclick = function (e) {
     e.preventDefault();
-    let url = "/caone/api/joke/all";
+    let url = "/CAone/api/joke/all";
     fetch(url)
             .then(res => res.json()) //in flow1, just do it
             .then(data => {
                 // Inside this callback, and only here, the response data is available
                 console.log("data", data);
                 JOKECONTENT.innerHTML = jokesToTable(data);
+                JOKECONTENT.innerHTML += jokeByIdButton();
                 /* data now contains the response, converted to JavaScript
                  Observe the output from the log-output above
                  Now, just build your DOM changes using the data*/
